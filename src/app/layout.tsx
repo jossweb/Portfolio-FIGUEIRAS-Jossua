fd47c1688e52fbe9a0b616e8b4ef1e7b6c9d7970
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-01-17 18:39:34 +0100

    welcome page style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from 'next/font/local'
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const swiss = localFont({ src: '../font/SuisseIntl-BoldItalic.ttf' })
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${swiss.className} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,27 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-4xl font-bold">🚧 Portfolio FIGUEIRAS Jossua 🏗️</h1>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Power by nextjs.org
-        </a>
-      </footer>
-    </div>
+    <main className="flex flex-col items-center sm:items-start bg-[#000] min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
+      <section className="h-screen w-full flex items-center justify-center bg-cover bg-center bg-[url('../../public/img/welcome-bg.webp')]">
+        <h1 className="text-6xl font-bold text-white max-w-[70%] text-center">Welcome to the portfolio of FIGUEIRAS Jossua</h1>
+      </section>
+      <section className="bg-white h-screen opacity-80 rounded-xl w-full flex items-center justify-center">
+      </section>
+    </main>
   );
 }
