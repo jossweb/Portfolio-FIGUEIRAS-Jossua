55c70ab414923b826a3d48524eb2fda3527253cd
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-01-18 18:58:19 +0100

    create ai project part and add 3 new components : button, progress, Spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,10 @@ import Link from "next/link";
 import { useState } from "react";
 import Image from "next/image";
 import {WobbleCard} from "../components/ui/wobble-card";
+import { Spotlight } from "../components/ui/Spotlight"
+import { Progress } from "@/components/ui/progress"
+import { Button } from "../components/ui/button"
+import { Github } from 'lucide-react'
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -45,12 +49,10 @@ export default function Home() {
         <section className="h-screen w-full flex items-center justify-center bg-cover bg-center bg-[url('/img/welcome-bg.webp')]">
           <h1 className="text-6xl font-bold text-white max-w-[70%] text-center">Welcome to the portfolio of FIGUEIRAS Jossua</h1>
         </section>
-        <section className=" w-full p-4">
+        <section className=" w-full p-4" id="about-me">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-5xl w-full mx-auto">
             <WobbleCard
-            containerClassName="col-span-1 lg:col-span-2 h-full bg-red-900 min-h-[180px] lg:min-h-[150px]"
-            className=""
-          >
+            containerClassName="col-span-1 lg:col-span-2 h-full bg-orange-900 min-h-[180px] lg:min-h-[150px]">
             <div className="max-w-xs">
               <h2 className="text-left text-balance text-base md:text-lg lg:text-xl font-semibold tracking-[-0.015em] text-white">
                 Github
@@ -95,28 +97,50 @@ export default function Home() {
               <li>C</li>
             </ul>
           </WobbleCard>
-          <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-orange-900 min-h-[180px] lg:min-h-[150px]">
+          <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[200px] lg:min-h-[200px] mb-20">
             <div className="max-w-sm">
               <h2 className="max-w-sm md:max-w-lg text-left text-balance text-base md:text-lg lg:text-xl font-semibold tracking-[-0.015em] text-white">
-                Signup for blazing-fast cutting-edge state of the art Gippity AI
-                wrapper today!
+                About me
               </h2>
-              <p className="mt-2 max-w-[20rem] text-left text-sm/5 text-neutral-200">
-                With over 100,000 monthly active bot users, Gippity AI is the most
-                popular AI platform for developers.
+              <p className="mt-4 max-w-[20rem] text-left text-sm/5 text-neutral-200">
+              Hi, my name is Jossua. I’m currently studying computer science at the University of Tours in France, where I’m in my first year of a computer science degree for the 2024/25 academic year. I have a strong passion for hardware and emerging technologies, particularly in the field of AI.
               </p>
             </div>
             <Image
-              src="/linear.webp"
-              width={300}
-              height={300}
+              src="/img/profil-git.webp"
+              width={500}
+              height={350}
               alt="linear demo image"
-              className="absolute -right-6 md:-right-[20%] lg:-right-[10%] -bottom-6 object-contain rounded-2xl"
+              className="absolute -right-6 -bottom-6 object-contain rounded-2xl grayscale filter"
             />
           </WobbleCard>
         </div>
-        <p className="text-center text-white">Discovers my project</p>
+        <p className="text-center text-white mt-8 pb-2">Discover my projects</p>
+        <Image
+              src="/img/scroll-arrow.jpg"
+              width={50}
+              height={50}
+              alt="linear demo image"
+              className="mx-auto block"
+            />
         </section>
+        <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="white" />
+      <div className="p-4 max-w-7xl mx-auto relative z-10 w-full pt-20 md:pt-0 flex flex-col items-center">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Local Ai <br /> rediscover AI
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          discover my work on less polluting artificial intelligence tools that respect your confidentiality...
+        </p>
+        <Progress value={10} className="w-[60%] mt-8 mb-2" />
+        <p className="mt-2 text-sm text-neutral-400 mb-5">Progress: 10%</p>
+        <p className="mt-2 text-sm text-neutral-400">Current step: adding llama to the mac application with coreml</p>
+        <Button variant="outline" className="mt-8" onClick={() => window.open('https://github.com/jossweb', '_blank')}>
+          <Github className="mr-2 h-4 w-4" />View on GitHub
+        </Button>
+      </div>
+    </div>
       </main>
     </div>
   );
