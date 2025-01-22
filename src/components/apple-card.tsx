e67adaecb6b5c2fccc77cd639deca2c4a59e280d
The-Real-Dr-Joss <jossufigueiras@icloud.com>
2025-01-22 21:50:29 +0100

    adding latest image and fixing latest carousel bugs

--- src/components/apple-card.tsx
+++ src/components/apple-card.tsx
@@ -1,13 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import Image from 'next/image';
 import { Carousel, Card } from '@/components/ui/apple-card-carousel';
 
 interface RepoData {
-  path: string;
   name: string;
-  url: string;
   commits: number;
+  url: string;
 }
 
 const Content = ({ hrefs, category, txt }: { hrefs: string[], category: string, txt: string }) => {
@@ -17,10 +15,18 @@ const Content = ({ hrefs, category, txt }: { hrefs: string[], category: string, 
   useEffect(() => {
     async function fetchData() {
       try {
+        // Filter out empty hrefs
+        const validHrefs = hrefs.filter(href => href !== "");
+        
+        if (validHrefs.length === 0) {
+          setRepoData([]);
+          return;
+        }
+
         const response = await fetch('/data.json');
         const data = await response.json() as { repos: RepoData[] };
 
-        const repoDataArray = hrefs.map((href) => {
+        const repoDataArray = validHrefs.map((href) => {
           const strippedPath = href.replace('https://api.github.com/repos/', '');
           const normalUrl = `https://github.com/${strippedPath}`;
           const foundRepo = data.repos.find((repo) => repo.url === normalUrl);
@@ -40,35 +46,32 @@ const Content = ({ hrefs, category, txt }: { hrefs: string[], category: string, 
 
     fetchData();
   }, [hrefs]);
-
-  if (error) {
-    return <div>{error}</div>;
-  }
-
   return (
     <div className="text-white text-lg">
-      <table className="mx-auto mt-10 w-[90%] text-left mb-10 rounded-lg border-separate border-spacing-0 border-2 border-white overflow-hidden">
-        <thead>
-          <tr>
-            <th className="p-2 border-b-2 border-white">Repository</th>
-            <th className="p-2 border-b-2 border-white">Commits</th>
-          </tr>
-        </thead>
-        <tbody>
-          {repoData.map((repo, index) => (
-            <tr
-              key={repo.url}
-              onClick={() => window.open(repo.url, '_blank')}
-              className="hover:bg-gray-700 cursor-pointer"
-            >
-              <td className="p-2 border-b border-white">{repo.name}</td>
-              <td className="p-2 border-b border-white">{repo.commits}</td>
+      {repoData && repoData.length > 0 ? (
+        <table className="mx-auto mt-10 w-[90%] text-left mb-10 rounded-lg border-separate border-spacing-0 border-2 border-white overflow-hidden">
+          <thead>
+            <tr>
+              <th className="p-2 border-b-2 border-white">Repository</th>
+              <th className="p-2 border-b-2 border-white">Commits</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {repoData.map((repo) => (
+              <tr
+                key={repo.url}
+                onClick={() => window.open(repo.url, '_blank')}
+                className="hover:bg-gray-700 cursor-pointer"
+              >
+                <td className="p-2 border-b border-white">{repo.name}</td>
+                <td className="p-2 border-b border-white">{repo.commits}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : null}
       <p className="text-justify">{txt}</p>
-      {error && <p>{error}</p>}
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   );
 };
@@ -93,26 +96,25 @@ const data = [
     title: "TopTech",
     src: "/img/toptech.webp",
     hrefs: ["https://api.github.com/repos/jossweb/Toptech"],
-    content: <Content hrefs={["https://api.github.com/repos/jossweb/Toptech", "https://api.github.com/repos/jossweb/Calculatrice", "https://api.github.com/repos/jossweb/Moniteur"]} category="School project - web" txt="TopTech was a team project in our computer science class during the 2022-23 school year. This website uses HTML/CSS/JS for the front-end and PHP for the server-side, particularly for the registration and login features." />,
+    content: <Content hrefs={["https://api.github.com/repos/jossweb/Toptech"]} category="School project - web" txt="TopTech was a team project in our computer science class during the 2022-23 school year. This website uses HTML/CSS/JS for the front-end and PHP for the server-side, particularly for the registration and login features." />,
   },
   {
     category: "Web",
-    title: "Oversim : help simracers with ai",
+    title: "Oversim",
     src: "/img/oversim.webp",
-    hrefs: ["https://api.github.com/repos/jossweb/Stable-Diffusion-M-Chips"],
-    content: <Content hrefs={["https://api.github.com/repos/jossweb/Stable-Diffusion-M-Chips"]} category="Web" txt="Oversim is a project aimed at helping sim racers through technologies like AI, with features such as setup generation. Unfortunately, this project is currently on hold to prioritize the development of other projects, but it has not been abandoned" />,
+    content: <Content hrefs={[""]} category="Web" txt="Oversim is a project aimed at helping sim racers through technologies like AI, with features such as setup generation. Unfortunately, this project is currently on hold to prioritize the development of other projects, but it has not been abandoned" />,
   },
   {
     category: "C# Project",
     title: "All my C# apps",
     src: "/img/csharp.webp",
-    hrefs: ["https://api.github.com/repos/jossweb/money-management", "https://api.github.com/repos/jossweb/Calculatrice", "https://api.github.com/repos/jossweb/Moniteur"],
-    content: <Content hrefs={["https://api.github.com/repos/jossweb/Stable-Diffusion-M-Chips"]} category="C# Project" txt="I started programming thanks to a C# (or C-Sharp) course on the Udemy platform. This course helped me understand how a programming language works, as well as development environments. Following this training, I worked on personal projects involving small applications created using the same C# language.”" />,
+    hrefs: ["https://api.github.com/repos/jossweb/Calculatrice", "https://api.github.com/repos/jossweb/Moniteur"],
+    content: <Content hrefs={["https://api.github.com/repos/jossweb/Calculatrice", "https://api.github.com/repos/jossweb/Moniteur"]} category="C# Project" txt="I started programming thanks to a C# (or C-Sharp) course on the Udemy platform. This course helped me understand how a programming language works, as well as development environments. Following this training, I worked on personal projects involving small applications created using the same C# language.”" />,
   },
   {
     category: "School project - Python",
     title: "All my python scripts",
-    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "/img/python.webp",
     hrefs: ["https://api.github.com/repos/jossweb/Jeu-de-carte-NSI", "https://api.github.com/repos/jossweb/projet-NSI-illusion"],
     content: <Content hrefs={["https://api.github.com/repos/jossweb/Jeu-de-carte-NSI", "https://api.github.com/repos/jossweb/projet-NSI-illusion"]} category="School project - Python" txt="Python is an ideal language for beginners, which is why it is taught in French high schools for computer science classes. This allowed me to work on some very simple projects in an academic setting." />,
   },
@@ -120,7 +122,7 @@ const data = [
 
 export default function AppleCardsCarouselDemo() {
   const cards = data.map((card, index) => <Card key={card.src} card={card} index={index} />);
-
+  
   return (
     <div className="w-full h-full py-20 bg-black">
       <h2 className="max-w-7xl pl-4 mx-auto text-xl md:text-5xl font-bold text-white font-sans">
